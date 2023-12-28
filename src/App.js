935ba396ec8c6cfd9ce756d2c8286a39ff53164d
Kaleshia98 <kaleshia.lee@usaa.com>
2023-12-28 17:10:24 -0600

    final touches to application

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, {useEffect, useState} from 'react';
 
-import HarryPotterList from './components/HarryPotterList.jsx';
+import HarryPotterList from './components/list/HarryPotterList.jsx';
 import './css/App.css';
 
 function App() {
   const [characters, setCharacters] = useState([]);
   const [displayedCharacters, setDisplayedCharacters] = useState([]);
+  const [selectedHouse, setSelectedHouse] = useState('');
   const houses = ['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw'];
 
   useEffect(() => {
-    fetch('https://hp-api.onrender.com/api/characters')
+    fetch('https://hp-api.onrender.com/api/characters/students')
     .then((res) => res.json())
     .then((hpCharacters) => {
       console.log(hpCharacters);
@@ -19,6 +20,8 @@ function App() {
 
   const handleCharacters = (e) => {
     let currentHouse = e.target.innerText;
+    setSelectedHouse(currentHouse);
+    console.log("ahhhhhh",selectedHouse);
     const newCharacters = [...characters];
     const selectedCharacters = newCharacters.filter(character => { 
       return character.house === currentHouse;
@@ -27,7 +30,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className={selectedHouse}>
         <h1 className='intro'>Harry Potter Characters</h1>
         <section className='nav'>
           {houses.map((house) => {
--- src/components/list/HarryPotterList.jsx
+++ src/components/list/HarryPotterList.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./HarryPotterList.css";
 
 function HarryPotterList({ characters }) {
-  console.log("thisss is harry potter list", characters.map);
+  // console.log("thisss is harry potter list", characters.map);
   return characters.map(({ image, name }) => {
     return (
       <div className="characterContainer" key={name}>
